2412c6d102dd7ba489c38bd8449997349d84509e
Hans-Peter Lerjen <hplerjen@gmx.net>
2023-12-17 22:14:08 +0100

    Status 17.12.2023

--- src/app/pages/address.tsx
+++ src/app/pages/address.tsx
@@ -2,7 +2,7 @@
 import { Button, Card, CardContent, TextField, Typography } from '@mui/material'
 import { observer } from 'mobx-react-lite'
 import React, { FormEvent, useState } from 'react'
-import "./cart.css";
+import "./card.css";
 import { useRootStore } from '../state/root-store';
 
 export const Address =  observer(() => {
