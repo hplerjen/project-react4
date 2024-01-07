0bdbd76aaca138abe002d6c833a6781e2f2b94eb
Hans-Peter Lerjen <hplerjen@gmx.net>
2024-01-07 21:40:12 +0100

    State 07.01.2024

--- src/__tests__/order-store_test.ts
+++ src/__tests__/order-store_test.ts
@@ -1,26 +1,45 @@
-
 import 'jest';
+import { describe, expect, test } from '@jest/globals';
 
 import { OrderStore } from "../app/state/order-store";
 import { useRootStore } from "../app/state/root-store";
+import { ProductStore } from 'src/app/state/product-store';
 
-/* it("increments counter", () => {
+//test fails
+test("number of products", () => {
     
     const orderStore = new OrderStore(useRootStore());
     const productsInCart = new Map<string, number>();
-    productsInCart.set("prodId=1", 50); 
-    productsInCart.set("prodId=2", 100);
+    productsInCart.set("1", 2); 
+    productsInCart.set("2", 4);
     
     orderStore.currentOrder =  {
         id: "",
         userId: "",
-        productsInCart : new Map <string,  number>(),
+        productsInCart : productsInCart,
         numberOfProducts: 0,
         costTotalWithoutPostage: 0}
     orderStore.calculateNumberOfProductsInCar();
     expect(orderStore.currentOrder.numberOfProducts === 2);
-    
-    });*/
+
+    const productStore = new ProductStore(useRootStore());
+    const prod1 = {
+        id: "",
+        title: "",
+        description: "",
+        stock: 10,
+        price: 10}
+        const prod2 = {
+            id: "",
+            title: "",
+            description: "",
+            stock: 5,
+            price: 5}
+        
+    productStore.add([prod1, prod2]);
+    orderStore.calculateTotalCostsInCart()
+    expect(orderStore.currentOrder.costTotalWithoutPostage === 30);
+    });
     
 
 
